4e63f5ca54d5b665bac3b1c7f3bb1b41a8d7d050
Xavier <andrewhaeffner@outlook.com>
2018-11-10 14:39:10 -0500

    joyop : improve user experience

--- scripts/joyop.py
+++ scripts/joyop.py
@@ -5,20 +5,17 @@ ackermann_drive_joyop.py:
     A ros joystick teleoperation script for ackermann steering based robots
 '''
 
-__author__ = 'George Kouros'
-__license__ = 'GPLv3'
-__maintainer__ = 'George Kouros'
-__email__ = 'gkourosg@yahoo.gr'
-
 import rospy
+import math
 from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import Joy
 import sys
 
+
 class AckermannDriveJoyop:
 
     def __init__(self, args):
-        if len(args)==1 or len(args)==2:
+        if len(args) == 1 or len(args) == 2:
             self.max_speed = float(args[0])
             self.max_steering_angle = float(args[len(args)-1])
             cmd_topic = 'ackermann_cmd'
@@ -31,32 +28,55 @@ class AckermannDriveJoyop:
             self.max_steering_angle = 1.0
             cmd_topic = 'ackermann_cmd'
 
-        self.speed = 0
-        self.steering_angle = 0
+        self.speed = 0.0
+        self.steering_angle = 0.0
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
         self.drive_pub = rospy.Publisher(cmd_topic, AckermannDriveStamped,
                                          queue_size=1)
-        rospy.Timer(rospy.Duration(1.0/50.0), self.pub_callback, oneshot=False)
-        rospy.loginfo('ackermann_drive_joyop_node initialized')
+	self.armed = False
+	self.forward_active = False
+	self.reverse_active = False
 
     def joy_callback(self, joy_msg):
-        self.speed = joy_msg.axes[3] * self.max_speed;
-        self.steering_angle = joy_msg.axes[2] * self.max_steering_angle;
+		self.forward_active = joy_msg.axes[4] < 0.99
+		self.reverse_active = joy_msg.axes[5] < 0.99
+		if(joy_msg.axes[4] < -0.5 and joy_msg.axes[5] < -0.5):
+			self.armed = True
+	        self.print_state()
+		if not self.armed:
+			return
+		if (self.forward_active and self.reverse_active):
+			# Brake
+			self.speed = 0.0
+		elif (self.reverse_active):
+			# Reverse
+			self.speed = -(joy_msg.axes[5] - 1.0)/2.0 * self.max_speed
+		elif (self.forward_active):
+			# Forward
+			self.speed = (joy_msg.axes[4] - 1.0)/2.0 * self.max_speed
+		else:
+			# Coast
+			self.speed = float('nan')  # TODO : use nan
 
+		# Steering
+		if abs(joy_msg.axes[0]) > 0.1:
+	        	self.steering_angle = - joy_msg.axes[0] * self.max_steering_angle
+		else:
+			self.steering_angle = 0.0
 
-    def pub_callback(self, event):
-        ackermann_cmd_msg = AckermannDriveStamped()
-        ackermann_cmd_msg.drive.speed = self.speed
-        ackermann_cmd_msg.drive.steering_angle = self.steering_angle
-        self.drive_pub.publish(ackermann_cmd_msg)
-        self.print_state()
+		# Publish control commands
+		ackermann_cmd_msg = AckermannDriveStamped()
+		ackermann_cmd_msg.drive.speed = self.speed
+		ackermann_cmd_msg.drive.steering_angle = self.steering_angle
+		self.drive_pub.publish(ackermann_cmd_msg)
 
     def print_state(self):
         sys.stderr.write('\x1b[2J\x1b[H')
         rospy.loginfo('\x1b[1M\r'
+                      '\033[34;1mArmed: \033[32;1m%s, '
                       '\033[34;1mSpeed: \033[32;1m%0.2f m/s, '
                       '\033[34;1mSteering Angle: \033[32;1m%0.2f rad\033[0m',
-                      self.speed, self.steering_angle)
+                      self.armed, self.speed if not math.isnan(self.speed) else 0.0, self.steering_angle)
 
     def finalize(self):
         rospy.loginfo('Halting motors, aligning wheels and exiting...')
